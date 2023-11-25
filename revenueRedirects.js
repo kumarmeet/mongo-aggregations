3edb16d38d32777ab0698585404277f2a95d3d11
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2023-11-25 17:52:14 +0530

    Create revenueRedirects.js

---
+++ revenueRedirects.js
@@ -0,0 +1,100 @@
+const aggregation = (pageNumber, pageSize) => {
+    return [
+        {
+          '$match': {
+            'type': 'CREDIT',
+            paymentStatus: "SUCCESS"
+          }
+        }, {
+          '$project': {
+            'period': {
+              '$dateToString': {
+                'format': format,
+                'date': '$date'
+              }
+            },
+            'price': 1,
+            'userId': 1,
+            'featuredType': 1,
+            'createdAt': 1
+          }
+        }, {
+          '$lookup': {
+            'from': 'users',
+            'localField': 'userId',
+            'foreignField': '_id',
+            'pipeline': [
+              {
+                '$project': {
+                  '_id': 0,
+                  'fname': 1,
+                  'lname': 1,
+                  'isPremium': 1,
+                  'phoneNumber': 1,
+                  'regDate': 1,
+                  'period': 1,
+                  'accountStatus': 1,
+                  'phoneNumber': 1
+                }
+              }
+            ],
+            'as': 'user_details'
+          }
+        }, {
+          '$unwind': {
+            'path': '$user_details'
+          }
+        }, {
+          '$addFields': {
+            'username': {
+              '$concat': [
+                '$user_details.fname', ' ', '$user_details.lname'
+              ]
+            }
+          }
+        }, {
+          '$lookup': {
+            'from': 'userimgs',
+            'localField': 'userId',
+            'foreignField': 'userId',
+            'pipeline': [
+              {
+                '$project': {
+                  '_id': 0,
+                  'photoUrl': 1
+                }
+              }
+            ],
+            'as': 'userImg'
+          }
+        }, {
+          '$set': {
+            'photoUrl': {
+              '$arrayElemAt': [
+                '$userImg.photoUrl', 0
+              ]
+            }
+          }
+        }, {
+          '$group': {
+            '_id': '$period',
+            'count': {
+              '$push': {
+                'username': '$username',
+                'photoUrl': '$photoUrl',
+                'isPremium': '$user_details.isPremium',
+                'regDate': '$user_details.regDate',
+                'accountStatus': '$user_details.accountStatus',
+                'period': '$period',
+                'price': '$price',
+                'phoneNumber': '$user_details.phoneNumber',
+                'featuredType': '$featuredType',
+                'paymentDate': '$createdAt'
+              }
+            }
+          }
+        }
+      ]
+}
+
+module.exports = aggregation;
